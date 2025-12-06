390260564df3f9a65b2c7dd586a6d4ec06c9fad5
Assaka <2682728781@qq.com>
2025-12-06 16:06:32 +0800

    finish MapExercises

--- src/MapExercises.java
+++ src/MapExercises.java
@@ -1,5 +1,7 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
@@ -7,7 +9,13 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> m = new HashMap<>();
+        char c = 'a';
+        for(int i = 1; i < 27; i++){
+            m.put(c, i);
+            c++;
+        }
+        return m;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +23,24 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> squares = new HashMap<>();
+        for(int i : nums){
+            squares.put(i, i*i);
+        }
+        return squares;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> count_words = new TreeMap<>();
+        for(String s : words){
+            if(!count_words.containsKey(s)){
+                count_words.put(s, 1);
+            }else{
+                count_words.put(s, count_words.get(s)+1);
+            }
+        }
+        return count_words;
     }
 }
